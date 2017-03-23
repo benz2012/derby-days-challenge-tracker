125d5f30302c6c8593b0714f322cc56c3fd58ca9
Ben Zenker <benz2012@Bens-MacBook-Pro.local>
2017-03-23 14:43:48 -0400

    visual changes

--- src/components/challengeChild.js
+++ src/components/challengeChild.js
@@ -7,7 +7,7 @@ export default class ChallengeChild extends React.Component {
   pickChild() {
     const { number, data } = this.props
     const challenges = {
-      0: <Challenge0 data={data} />
+      0: <Challenge0 data={data} />,
       1: <Challenge1 data={data} />
     }
     return number in challenges ? challenges[number] : null

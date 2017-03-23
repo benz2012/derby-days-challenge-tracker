cd25b30b8ca1492979400271fb51c0b98b165ad7
Ben Zenker <benz2012@Bens-MacBook-Pro.local>
2017-03-23 12:40:37 -0400

    added showing and hiding capability

--- src/components/challenge.js
+++ src/components/challenge.js
@@ -3,14 +3,30 @@ import React from 'react'
 import Api from './api'
 
 export default class Challenge extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      visibleLabel: 'hide ▲'
+    }
+  }
+  changeVisible() {
+    const { visibleLabel } = this.state
+    if (visibleLabel === 'hide ▲') {
+      this.setState({visibleLabel: 'show ▼'})
+    } else if (visibleLabel === 'show ▼') {
+      this.setState({visibleLabel: 'hide ▲'})
+    }
+  }
   render() {
     const { number } = this.props
+    const { visibleLabel } = this.state
     return(
       <div className='row'>
         <div className='col-lg-12'>
           <h1 style={{marginBottom: '5px'}}>Challenge {number}</h1>
+          <small><a onClick={this.changeVisible()}>{visibleLabel}</a></small>
           <hr style={{marginTop: '12px'}}/>
-          <Api number={number} />
+          { visibleLabel === 'hide ▲' && <Api number={number} />}
         </div>
       </div>
     )

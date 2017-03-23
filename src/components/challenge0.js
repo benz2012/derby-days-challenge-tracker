5c0d8c3809c98bcb05d80ee613971c42eda2fb76
Ben Zenker <benz2012@Bens-MacBook-Pro.local>
2017-03-23 14:47:28 -0400

    visual changes

--- src/components/challenge0.js
+++ src/components/challenge0.js
@@ -8,7 +8,7 @@ export default class Challenge0 extends React.Component {
     Object.keys(teamObjs).forEach(teamKey => {
       const team = teamObjs[teamKey]
       const teamEl = <TeamProfile key={team.Team} name={team.Team}
-        number={0} chapter={team.Chapter} raised={team.Raised}} />
+        number={0} chapter={team.Chapter} raised={team.Raised} />
       teamElements.push(teamEl)
     })
     return teamElements

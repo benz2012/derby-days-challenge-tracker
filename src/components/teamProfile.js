a1d5b9811b8faec349f9dda1863bc7bda78b2a4e
Ben Zenker <benz2012@Bens-MacBook-Pro.local>
2017-03-23 14:23:50 -0400

    visual changes

--- src/components/teamProfile.js
+++ src/components/teamProfile.js
@@ -29,7 +29,7 @@ export default class TeamProfile extends React.Component {
     const colors = this.colors(team)
     return {
       // color: 'rgb(51, 51, 51)',
-      color: 'white',
+      color: team == 'Zeta Tau Alpha'? colors[1] : 'white',
       // backgroundColor: 'rgb(230,230,230)',
       backgroundColor: colors[0],
       padding: '10px 5px 10px 5px',
